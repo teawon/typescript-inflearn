88d3e3304e8380afcd18b51d75d2629b14bf6581
teawon <xodnjsvkvk9@gmail.com>
2024-08-13 01:11:43 +0900

    1장 타입스크립트 컴파일러 옵션 설정
    tsconfig.json

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,2 @@
 import { testFunction } from "./testFuction";
-
 testFunction();
---
+++ src/strictExample.ts
@@ -0,0 +1,4 @@
+export const hello = (message) => {
+  console.log(message);
+};
+//  strict가 true라면 'message' 매개 변수에는 암시적으로 'any' 형식이 포함됩니다.ts(7006) 에러 발생
